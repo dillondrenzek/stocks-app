5cb2fd141fd6872a05ea83594c9681ff5abf47a3
Dillon Drenzek <dr.enzek@gmail.com>
2017-06-04 00:52:16 -0700

    version 0.1.7 - closes #27 - ActivePortfolioService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import * as tokens from './core/tokens';
   ],
   providers: [
     { provide: tokens.USE_LOCAL_STORAGE, useValue: true },
-    { provide: tokens.APP_VERSION, useValue: 'v0.1.6' }
+    { provide: tokens.APP_VERSION, useValue: 'v0.1.7' }
   ],
   bootstrap: [AppComponent]
 })
