04f1d130cd302f4f91c47f2ad00256bfc6257fc4
Dillon Drenzek <dr.enzek@gmail.com>
2017-06-04 00:05:50 -0700

    comment portfolio

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -10,8 +10,24 @@ import { Holding } from './holding';
  * Portfolio is a basic grouping of Holdings
  */
 export interface Portfolio {
+
+  /**
+   * Unique identifier
+   */
   id: string;
+
+  /**
+   * Array of holdings
+   */
   holdings: Holding[];
+
+  /**
+   * Date last modified, changed on save
+   */
   dateModified: string;
+
+  /**
+   * Date created, established on creation
+   */
   dateCreated: string;
 }
